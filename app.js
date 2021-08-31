73dc4eb587cd47367b9f0180d47f94196c1810c9
Ian Ro <ianrogers2422@gmail.com>
2021-08-30 20:50:42 -0400

    modal kind of works

--- app.js
+++ app.js
@@ -1,26 +1,18 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+
+let imageClick = document.querySelector('.gallery-img');
+let modalClose = document.querySelector('.modal-close');
+let wrapper = document.querySelector('.modal-wrapper');
 
 
-burger.addEventListener('click', () => {
-    nav.classList.toggle('nav-active');
-    navLinks.forEach((link, index) => {
-        if(link.style.animation){
-            link.style.animation = ''
-        } else{
-         link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-        }
- });
- burger.classList.toggle('toggle');
+imageClick.addEventListener('click', function(){
+    wrapper.classList.add('active');
+})
+
+modalClose.addEventListener('click', function(){
+      wrapper.classList.remove('active');
 });
 
-}
-
-navSlide();
-
-//Modal
+/*
 
 const modalOp = document.querySelector(".overlay");
 const modal = document.querySelector(".modal");
@@ -43,3 +35,4 @@ function outsideClick(e){
          modal.style.display = 'none';
      }
 }
+*/
